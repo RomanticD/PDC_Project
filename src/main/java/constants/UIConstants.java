7ac2e5674fc7695bbb89dd1c7c204c2eb7763ec6
KazamiRenge <110608596+KazamiRenge@users.noreply.github.com>
2023-10-10 16:41:54 +0800

    test4

--- src/main/java/constants/UIConstants.java
+++ src/main/java/constants/UIConstants.java
@@ -13,5 +13,4 @@ public class UIConstants {
     public static final int REGISTER_FIELD_WIDTH = 200;
     public static final int REGISTER_FIELD_HEIGHT = 20;
     public static final String LOGIN_BACKGROUND_IMAGE = "src/main/resources/login_gui_background.jpg";
-    public static final String REGISTER_BACKGROUND_IMAGE = "src/main/resources/login_gui_background.jpg";
 }
