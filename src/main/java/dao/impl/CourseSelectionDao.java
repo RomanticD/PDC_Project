3937877e69303403cbaf0567ef816aa219eff506
RomanticD <romantic-di@outlook.com>
2023-10-13 11:11:57 +0800

    Add Course Selection to DB

---
+++ src/main/java/dao/impl/CourseSelectionDao.java
@@ -0,0 +1,11 @@
+package dao.impl;
+
+import dao.CourseSelectionDaoInterface;
+
+public class CourseSelectionDao implements CourseSelectionDaoInterface {
+
+    @Override
+    public int[] getSelectedCourseId() {
+        return new int[0];
+    }
+}
