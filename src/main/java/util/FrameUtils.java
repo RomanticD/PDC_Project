dc12da62f0b831e490671ab496a84c8297800911
RomanticD <romantic-di@outlook.com>
2023-10-16 18:15:14 +0800

    Add method comments

--- src/main/java/util/FrameUtils.java
+++ src/main/java/util/FrameUtils.java
@@ -9,6 +9,12 @@ import java.lang.reflect.InvocationTargetException;
 
 public class FrameUtils {
 
+    /**
+     * Disposes the current frame and creates a new frame with the specified title and panel.
+     * @param title Title of the new frame.
+     * @param currentPanel Current panel to be disposed.
+     * @param newPanel New panel to be added to the new frame.
+     */
     public static void disposeCurrentFrameAndCreateNewFrame(String title, JPanel currentPanel, JPanel newPanel) {
         JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(currentPanel);
         frame.dispose();
@@ -21,6 +27,12 @@ public class FrameUtils {
         newFrame.setVisible(true);
     }
 
+    /**
+     * Disposes the current frame and creates a new frame with the specified title and panel.
+     * @param title Title of the new frame.
+     * @param currentFrame Current frame to be disposed.
+     * @param newPanel New panel to be added to the new frame.
+     */
     public static void disposeCurrentFrameAndCreateNewFrame(String title, JFrame currentFrame, JPanel newPanel) {
         currentFrame.dispose();
 
@@ -32,6 +44,10 @@ public class FrameUtils {
         newFrame.setVisible(true);
     }
 
+    /**
+     * Displays a dialog with the specified message.
+     * @param message Message to be displayed in the dialog.
+     */
     public static void showDialog(String message){
         Object[] options = {"OK"};
         JOptionPane.showOptionDialog(null, message,
@@ -39,6 +55,16 @@ public class FrameUtils {
                 null, options, options[0]);
     }
 
+    /**
+     * Adds a back button to the container panel that, when clicked, disposes the current frame and creates a new frame
+     * specified by the provided class and arguments.
+     *
+     * @param containerPanel   Container panel to which the back button will be added.
+     * @param layout           Layout of the container panel.
+     * @param currentFrame     Current frame to be disposed.
+     * @param backToFrameClass Class of the frame to be created when the back button is clicked.
+     * @param args             Arguments to be passed to the constructor of the frame being created.
+     */
     public static void addBackButton(JPanel containerPanel, SpringLayout layout, JFrame currentFrame, Class<? extends JFrame> backToFrameClass, Object... args) {
         JButton backButton = new JButton("Back");
         backButton.setFont(new Font("Dialog", Font.BOLD, 15));
@@ -66,6 +92,12 @@ public class FrameUtils {
         containerPanel.add(backButton);
     }
 
+    /**
+     * Adds a back button to the container panel with a specified action to be performed when clicked.
+     * @param containerPanel Container panel to which the back button will be added.
+     * @param layout Layout of the container panel.
+     * @param customAction Custom action to be performed when the back button is clicked.
+     */
     public static void addBackButtonWithCustomAction(JPanel containerPanel, SpringLayout layout, ActionListener customAction) {
         JButton backButton = new JButton("Back");
         backButton.setFont(new Font("Dialog", Font.BOLD, 15));
